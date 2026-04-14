42dfa3567d1f91fa59f335151f5f67c8af16b08f
Fabio Freitas <fbifreitas@gmail.com>
2026-04-14 18:15:01 -0300

    v1.2.57+77 - fix: alinhar contrato do config page no web ci

--- apps/web-backoffice/test/config_page.test.tsx
+++ apps/web-backoffice/test/config_page.test.tsx
@@ -8,6 +8,6 @@ test('backoffice config page exposes operational config route content', () => {
   const markup = renderToStaticMarkup(<BackofficeConfigPage />);
 
   assert.match(markup, /Configuracao dinamica de check-in/);
-  assert.match(markup, /Central de configuracao/);
+  assert.match(markup, /Central de configuracoes/);
   assert.match(markup, /Voltar ao dashboard/);
 });
